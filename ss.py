2e14d7c78aa981625e27b7b0281d124fb853b399
Jasmine Fitt <jasminerachelf@gmail.com>
2023-11-29 16:55:09 -0700

    ok

--- ss.py
+++ ss.py
@@ -26,8 +26,12 @@ sys.stdout = open(sys.stdout.fileno(), 'w', buffering=1)
 
 def handle_client(connection, address, chain_list, url):
     if not chain_list:
-        filename = f"{str(uuid.uuid4())}_{url.split('/')[-1]}"
-        subprocess.run(['wget', '-N', url])
+        # Extract the filename from the URL
+        filename = url.split('/')[-1]
+        print(f"Request: {url}")
+        print("Chainlist is empty")
+        print(f"Issuing wget for file {url}")
+        subprocess.run(['wget', '-O', filename, '-N', url])
         with open(filename, 'rb') as file:
             while True:
                 bytes_read = file.read(1024)
@@ -38,31 +42,42 @@ def handle_client(connection, address, chain_list, url):
     else:
         next_ss = random.choice(chain_list)
         chain_list.remove(next_ss)
-        next_host, next_port = next_ss.split()
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ss_socket:
-            ss_socket.connect((next_host, int(next_port)))
-            serialized_data = pickle.dumps([url] + chain_list)
-            ss_socket.sendall(serialized_data)
+        ss_info = next_ss.split()
+        
+        # Check if ss_info has enough values before unpacking
+        if len(ss_info) >= 2:
+            next_host, next_port = ss_info[0], ss_info[1]
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ss_socket:
+                ss_socket.connect((next_host, int(next_port)))
+                updated_chain_list = [f"{next_host} {next_port}"] + chain_list
+                serialized_data = pickle.dumps([url] + updated_chain_list)
+                ss_socket.sendall(serialized_data)
 
-            print(f'ss {next_host}, {next_port}:')
-            print(f'Request: {url}...')
-            print(f'chainlist is')
-            for ss_entry in chain_list:
-                print(ss_entry)
-            print(f'next SS is {next_ss}')
-            print('waiting for file...')
-            
-            while True:
-                data = ss_socket.recv(1024)
-                if not data:
-                    break
-                connection.sendall(data)
+                print(f'ss {next_host}, {next_port}:')
+                print(f'Request: {url}...')
+                print(f'Chainlist is')
+                for ss_entry in updated_chain_list:
+                    print(ss_entry)
+                print(f'Next SS is {next_ss}')
+                print('Waiting for file...')
+
+                save_file(filename, ss_socket)
+                os.remove(filename)
+    
+                print('File received from the second SS')
 
             print('Relaying file...')
     
     print('Goodbye!')
     connection.close()
 
+def save_file(filename, conn):
+    with open(filename, 'wb') as file:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            file.write(data)
 
 def main(port):
     host = socket.gethostname()
@@ -80,7 +95,7 @@ def main(port):
                     data = pickle.loads(data)
                     url = data[0]
                     chain_list = data[1:]
-                threading.Thread(target=handle_client, args=(conn, addr, chain_list, url)).start()
+                    threading.Thread(target=handle_client, args=(conn, addr, chain_list, url)).start()
         except KeyboardInterrupt:
             print("\nServer is shutting down.")
         finally:
