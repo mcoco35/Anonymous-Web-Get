b71fc500f359f3c440256c83201cd9131853eba3
Mason <mcoco35@colostate.edu>
2023-11-28 19:48:24 -0700

    First push

--- awget.py
+++ awget.py
@@ -12,5 +12,52 @@
 ###############################################
 # MUST KEEP: sets printing butter to 1, so it will always flush. allows the autograder to work. 
 import sys
+import socket
+import os
+import random
 sys.stdout = open(sys.stdout.fileno(), 'w', buffering=1)
 ###############################################
+
+def read_chain_file(chainfile):
+    with open(chainfile, 'r') as file:
+        lines = file.readlines()
+    num_ss = int(lines[0].strip())
+    ss_list = [line.strip() for line in lines[1:num_ss+1]]
+    return ss_list
+
+def save_file(filename, conn):
+    with open(filename, 'wb') as file:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            file.write(data)
+
+def main(url, chainfile):
+    if not chainfile:
+        if os.path.exists("chaingang.txt"):
+            chainfile = "chaingang.txt"
+        else:
+            print("Error: Chainfile not found.")
+            sys.exit(1)
+
+    ss_list = read_chain_file(chainfile)
+    selected_ss = random.choice(ss_list)
+    ss_list.remove(selected_ss)
+    host, port = selected_ss.split()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, int(port)))
+        s.sendall(f"{url}\n{len(ss_list)}\n" + "\n".join(ss_list).encode())
+
+        filename = url.split('/')[-1] if '/' in url else 'index.html'
+        save_file(filename, s)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: awget.py [URL] [-c chainfile]")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    chainfile = sys.argv[3] if len(sys.argv) > 3 and sys.argv[2] == '-c' else None
+    main(url, chainfile)
--- ss.py
+++ ss.py
@@ -13,5 +13,63 @@
 ###############################################
 # MUST KEEP: sets printing butter to 1, so it will always flush. allows the autograder to work. 
 import sys
+import socket
+import threading
+import os
+import random
+import subprocess
+import argparse
 sys.stdout = open(sys.stdout.fileno(), 'w', buffering=1)
 ###############################################
+
+def handle_client(connection, address, chain_list, url):
+    if not chain_list:
+        subprocess.run(['wget', url])
+        filename = url.split('/')[-1]
+        with open(filename, 'rb') as file:
+            while True:
+                bytes_read = file.read(1024)
+                if not bytes_read:
+                    break
+                connection.sendall(bytes_read)
+        os.remove(filename)
+    else:
+        next_ss = random.choice(chain_list)
+        chain_list.remove(next_ss)
+        next_host, next_port = next_ss.split()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ss_socket:
+            ss_socket.connect((next_host, int(next_port)))
+            ss_socket.sendall(f"{url}\n{len(chain_list)}\n" + "\n".join(chain_list).encode())
+            while True:
+                data = ss_socket.recv(1024)
+                if not data:
+                    break
+                connection.sendall(data)
+
+    connection.close()
+
+def main(port):
+    host = socket.gethostname()
+    print(f"Hostname: {host}\nPort: {port}")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+
+        try:
+            while True:
+                conn, addr = s.accept()
+                data = conn.recv(1024).decode()
+                url, num_ss, *chain_list = data.split('\n')
+                threading.Thread(target=handle_client, args=(conn, addr, chain_list, url)).start()
+        except KeyboardInterrupt:
+            print("\nServer is shutting down.")
+        finally:
+            s.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Stepping Stone for Anonymous Web Get")
+    parser.add_argument('-p', '--port', type=int, default=20000, help='Port number to listen on')
+    args = parser.parse_args()
+    
+    main(args.port)
